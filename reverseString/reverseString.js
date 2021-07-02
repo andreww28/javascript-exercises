c8e4fd5541986d148a1d9f3f00e88cd2649b5224
John Andrew <64731318+andreww28@users.noreply.github.com>
2021-07-02 08:47:06 +0800

    Update reverseString.js

--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+  reverse_str = "";
+  for(let i = str.length; i >= 0; i--){
+    reverse_str += str.charAt(i);
+  }
+  return reverse_str;
 };
 
 module.exports = reverseString;
