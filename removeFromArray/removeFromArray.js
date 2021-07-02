427e0629eb388fceec764e7737102363a323eb3e
John Andrew <64731318+andreww28@users.noreply.github.com>
2021-07-02 17:26:34 +0800

    Update removeFromArray.js

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,18 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, args1,args2,args3,args4,args5){
+  let i = 0;
+  let args_array = Array.from(arguments);
+  args_array.shift();
+  
+  while(i < arr.length){
+    console.log(args_array.includes(arr[i]), i)
+    if(args_array.includes(arr[i])){
+      arr.splice(i,1);
+      i = 0;
+      continue;
+    }
+    i++;
+  }
+  return arr;
 };
 
 module.exports = removeFromArray;
