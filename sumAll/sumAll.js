58f8751b09499752cd0052511cf806b4c7a66306
John Andrew <64731318+andreww28@users.noreply.github.com>
2021-07-02 17:49:39 +0800

    Added Solution

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,27 @@
-const sumAll = function() {
-
+const sumAll = function(first_num, second_num) {
+  let highest_num;
+  let lowest_num;
+  sum = 0;
+  
+  if((typeof first_num != 'number' || typeof second_num != 'number') || (first_num < 0 || second_num < 0)){
+    return 'ERROR';
+  }
+  
+  if(first_num > second_num){
+    highest_num = first_num;
+    lowest_num = second_num;
+  }else if(first_num < second_num){
+    highest_num = second_num;
+    lowest_num = first_num;
+  }
+  
+  
+  for(let i = lowest_num; i <= highest_num; i++){
+    sum += i;
+  }
+  
+  return sum;
+  
 };
 
 module.exports = sumAll;
