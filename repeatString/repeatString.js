0d58a79b567ec797127232e7446e37f5100d0d18
John Andrew <64731318+andreww28@users.noreply.github.com>
2021-07-02 08:36:09 +0800

    Update repeatString.js

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(str, counter) {
+  combine_str = "";
+  for(let i= 0; i < counter; i++){
+    combine_str+=str;
+  }
+  return combine_str;
 };
 
 module.exports = repeatString;
