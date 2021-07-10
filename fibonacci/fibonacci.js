f716dc75a5e179f96f33e47432171322cbe75959
John Andrew <64731318+andreww28@users.noreply.github.com>
2021-07-10 14:53:27 +0800

    Update fibonacci.js

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
-
+const fibonacci  = function(n){
+  let arr = [1,1]
+  
+  if(n == 1) return 1;
+  if(n < 1) return "OOPS";
+  
+  for(let i = 0; i < (n - 2); i++){
+    arr.push(arr[arr.length -1] + arr[arr.length -2]);
+    console.log(arr);
+  }
+  return arr[arr.length -1];
 };
 
 module.exports = fibonacci;
