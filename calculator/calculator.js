ba00b5a9baf9fa5d86b60e08b490e9eb4043af97
John Andrew <64731318+andreww28@users.noreply.github.com>
2021-07-10 13:23:45 +0800

    Update calculator.js

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(nums) {
+  if(nums.length === 0) return 0;
+  
+	return nums.reduce((total, num) => {
+	  return total + num;
+	});
 };
 
-const multiply = function() {
-
+const multiply = function(nums) {
+  if(nums.length === 0) return 0;
+  
+  return nums.reduce((total, num) => {
+    return total * num;
+  });
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+  return (n === 1 || n === 0) ? 1 : n * factorial(n-1);
 };
 
 module.exports = {
