b94fa2f2ed0b79ff4cee2f15a5ff1b96471e11aa
John Andrew <64731318+andreww28@users.noreply.github.com>
2021-07-10 14:10:08 +0800

    Update palindromes.js

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,19 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  let original = str.split(/[^(A-Z)^(a-z)]/).join('');
+  let reverse = function() {
+    let base = original.split('');
+    let reverse_str = "";
+    
+    for(let i = base.length - 1; i >= 0; i--){
+      reverse_str += base[i];
+    }
+    return reverse_str;
+  };
+  
+  console.log(original.toLowerCase());
+  console.log(reverse().toLowerCase());
+ 
+  return original.toLowerCase() === reverse().toLowerCase();
 };
 
 module.exports = palindromes;
