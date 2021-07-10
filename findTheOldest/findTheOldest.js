d7d12d932a4e8ea66d56b89cd6440ec35962fd6d
John Andrew <64731318+andreww28@users.noreply.github.com>
2021-07-10 15:43:56 +0800

    Update findTheOldest.js

--- findTheOldest/findTheOldest.js
+++ findTheOldest/findTheOldest.js
@@ -1,5 +1,7 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(persons) {
+  person_year = persons.map(person => (person.yearOfDeath == undefined) ? person['yearOfDeath'] = new Date().getFullYear() : person['yearOfDeath'] = person['yearOfDeath']);
+  console.log(persons);
+  return persons.sort((a,b) => (a.yearOfDeath - a.yearOfBirth) > (b.yearOfDeath - b.yearOfBirth) ? 1: -1)[persons.length - 1];
 };
 
 module.exports = findTheOldest;
